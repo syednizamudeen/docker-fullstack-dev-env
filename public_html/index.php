a8268ddc2c5287413166d0fa444e4b6cd24f46d8
Syed Nizamudeen <syednizamudeen@users.noreply.github.com>
2019-01-12 13:03:52 +0800

    Initial Commit
    Apache
    PHP
    MySQL
    phpmyadmin

---
+++ public_html/index.php
@@ -0,0 +1,30 @@
+<h1>Hello !!!</h1>
+<h4>Attempting MySQL connection from php...</h4>
+<?php
+$host = 'mysql';
+$user = 'root';
+$pass = 'rootpassword';
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected to MySQL successfully!";
+
+// $link = mysqli_connect($host, $user, $pass, "test");
+
+// if (!$link) {
+//     echo "Error: Unable to connect to MySQL." . PHP_EOL;
+//     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+//     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+//     exit;
+// }
+
+// echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+// echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+// mysqli_close($link);
+
+phpinfo();
+error_log('Test From Container PHP Error LOG...')
+?>
