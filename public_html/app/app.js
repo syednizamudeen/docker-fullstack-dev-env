738afc131cc294fa09b064d8e0d42e3976b949a9
syednizamudeen <nizam.programmer@gmail.com>
2020-03-28 15:59:51 +0800

    Login using passport-local

--- public_html/app/app.js
+++ public_html/app/app.js
@@ -1,21 +1,35 @@
-"use strict";
 const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
+// const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const passport = require("passport");
-const flash = require("connect-flash");
+// const flash = require("connect-flash");
 const session = require("express-session");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // Constants
 const HOST = "0.0.0.0";
 const PORT = 3000;
 
+mongoose.promise = global.Promise;
+
 // App
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello World with Express\n");
-});
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+app.use(
+  session({
+    secret: "passport-tutorial",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 mongoose
   .connect("mongodb://mongo:27017", {
@@ -31,19 +45,15 @@ mongoose
     console.log("Unable to connect to the mongodb instance. Error: ", reason);
   });
 
-let db = mongoose.connection;
+require("./models/User");
+require("./config/passport");
+app.use(require("./routes"));
 
-if (!db) console.error("Error connecting db");
-else console.log("Db connected successfully");
+app.get("/", (req, res) => {
+  res.send("Hello World with Express\n");
+});
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-app.use(bodyParser.json());
-
-let apiRoutes = require("./api-routes");
-app.use("/api", apiRoutes);
+// let apiRoutes = require("./api-routes");
+// app.use("/api", apiRoutes);
 
 app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`));
--- public_html/app/models/User.js
+++ public_html/app/models/User.js
@@ -1,24 +1,50 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+const { Schema } = mongoose;
+
+const UsersSchema = new Schema({
+  email: String,
+  hash: String,
+  salt: String
 });
 
-const User = mongoose.model("User", UserSchema);
+UsersSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString("hex");
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+    .toString("hex");
+};
 
-module.exports = User;
+UsersSchema.methods.validatePassword = function(password) {
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+    .toString("hex");
+  return this.hash === hash;
+};
+
+UsersSchema.methods.generateJWT = function() {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 60);
+
+  return jwt.sign(
+    {
+      email: this.email,
+      id: this._id,
+      exp: parseInt(expirationDate.getTime() / 1000, 10)
+    },
+    "secret"
+  );
+};
+
+UsersSchema.methods.toAuthJSON = function() {
+  return {
+    _id: this._id,
+    email: this.email,
+    token: this.generateJWT()
+  };
+};
+
+mongoose.model("Users", UsersSchema);
